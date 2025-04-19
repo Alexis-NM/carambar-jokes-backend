c4712467452768956295933e8418484c1ea29c4c
Alexis-NM <alexis.na.me@gmail.com>
2025-04-19 15:55:09 +0200

    Enable CORS for GitHub Pages frontend

--- src/app.js
+++ src/app.js
@@ -13,26 +13,16 @@ const FRONT_URL = process.env.FRONT_URL;
 
 const app = express();
 
-// CORS
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (origin === FRONT_URL) return callback(null, true);
-      callback(new Error("Not allowed by CORS"));
-    },
+    origin: FRONT_URL,
   })
 );
 
 app.use(express.json());
-
-// Swagger UI (doc)
 setupSwagger(app);
-
-// Routes
 app.use("/api/v1/jokes", jokeRoutes);
 
-// Connexion
 (async () => {
   try {
     await sequelize.authenticate();
