576e787ccb99302aa03ac97392db0c02b5bded65
Alexis-NM <alexis.na.me@gmail.com>
2025-04-19 15:50:34 +0200

    Enable CORS for GitHub Pages frontend

--- src/app.js
+++ src/app.js
@@ -1,28 +1,47 @@
+// src/app.js
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import sequelize from "./config/database.js";
 import jokeRoutes from "./routes/jokeRoutes.js";
 import { setupSwagger } from "./docs/swagger.js";
 
 dotenv.config();
 
+const PORT = process.env.PORT;
+const FRONT_URL = process.env.FRONT_URL;
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+// CORS
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (origin === FRONT_URL) return callback(null, true);
+      callback(new Error("Not allowed by CORS"));
+    },
+  })
+);
 
 app.use(express.json());
 
-// Swagger UI
+// Swagger UI (doc)
 setupSwagger(app);
 
 // Routes
 app.use("/api/v1/jokes", jokeRoutes);
 
+// Connexion
 (async () => {
   try {
     await sequelize.authenticate();
+    console.log("✅ Connected to the database.");
     await sequelize.sync({ alter: true });
+    console.log("✅ Database synchronized.");
+
     app.listen(PORT, () => {
-      console.log(`🚀 Server listening on http://localhost:${PORT}`);
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
       console.log(
         `📖 Swagger UI available at http://localhost:${PORT}/api-docs`
       );
